33073cad5cd3deae6d2a17b022258f77d7b029d3
yvvdwe <82168424+yvvdwe@users.noreply.github.com>
2023-01-07 16:13:13 +0100

    fix syntax error :-(

--- dist/index.js
+++ dist/index.js
@@ -6986,7 +6986,7 @@ class Deployment {
         )
       }
       if( process.env.SHOW_OIDC )
-	  core.info("OIDC: " + btoa( idToken );
+	  core.info("OIDC: " + btoa( idToken ));
       const artifactUrl = `${artifactRawUrl}&%24expand=SignedContent`
       const payload = {
         artifact_url: process.env.ARTIFACT_URL || artifactUrl,
